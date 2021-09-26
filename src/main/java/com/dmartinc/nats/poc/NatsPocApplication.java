60ea37928c5bee03ab20900101aab1e5ee7571fa
dmartinc <danny90zgz@gmail.com>
2021-09-26 21:24:18 +0200

    Initialize spring boot project

---
+++ src/main/java/com/dmartinc/nats/poc/NatsPocApplication.java
@@ -0,0 +1,13 @@
+package com.dmartinc.nats.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmartinc/nats/poc/NatsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmartinc.nats.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
